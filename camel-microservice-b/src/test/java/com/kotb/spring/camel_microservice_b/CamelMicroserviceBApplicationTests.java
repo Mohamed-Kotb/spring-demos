d268af6b09d6c317835d4ebeff6b6b04bf0e5d42
Mohamed Kotb <mohamedkotb@eand.com>
2025-09-26 14:27:26 +0400

    add Camel demo

---
+++ camel-microservice-b/src/test/java/com/kotb/spring/camel_microservice_b/CamelMicroserviceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotb.spring.camel_microservice_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroserviceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
